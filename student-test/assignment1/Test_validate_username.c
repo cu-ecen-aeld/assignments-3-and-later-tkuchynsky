3195a3e57cd3bb3d60192683de36407d28b4b134
Taras Kuchynskyy <tkuchynsky@gmail.com>
2022-12-31 19:22:10 +0100

    Assignment 1

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,13 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    //TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+
+    const char * myUsername = my_username();
+    char * usernameFromConfFile = malloc_username_from_conf_file();
+
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(myUsername, usernameFromConfFile, "Nanes are not equal!");
+
+    free(usernameFromConfFile);
+
 }
